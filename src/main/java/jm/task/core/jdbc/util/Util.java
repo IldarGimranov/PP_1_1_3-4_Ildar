72a97368e9438daafb58123c94425ff1d3e88531
IldarGimranov <164064433+IldarGimranov@users.noreply.github.com>
2024-07-03 01:02:53 +0500

    Version2

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,7 +3,6 @@ package jm.task.core.jdbc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Util {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
@@ -11,6 +10,7 @@ public class Util {
     private static final String PASSWORD = "root";
     private static final String CON_URL = "jdbc:mysql://localhost:3306/mydbtest";
     private static Connection conn = null;
+
     private Util() {
     }
 
